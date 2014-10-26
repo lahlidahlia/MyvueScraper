f09d9820e316ecae3098c807b56442539ca3e077
Tin Huynh <tinn_1998@yahoo.com>
2014-10-26 14:52:35 -0700

    Turn all texts from the page into one string, instead of a list of strings. Now we can parse it better.

--- MyvueScraper.py
+++ MyvueScraper.py
@@ -5,7 +5,7 @@ PASSWORD = raw_input('Password pls: ')
 
 login_url = 'https://myvue.hsd.k12.or.us/Login_Student_PXP.aspx'
 destination_url = 'https://myvue.hsd.k12.or.us/Home_PXP.aspx'
-
+math_url = "https://myvue.hsd.k12.or.us/PXP_Gradebook.aspx?AGU=0&DGU=0&VDT=0&CID=866043FD-4A76-4AE3-A97E-EA655AF8117B&MK=3DCC8162-7F92-4317-AFBF-E5F464772BA0&OY=C26A5BDE-0387-4388-8855-1B81AE4C65F1&GP=B1C73921-DEB6-41E8-AA79-514267833B0A"
 
 #Poll the login page and get the appropriate form data
 get_payload = requests.get(login_url)
@@ -35,13 +35,23 @@ while True:
     sys.stdout.flush()
     time.sleep(5)
     
-get = s.get(destination_url) #Get the page you want
+get = s.get(math_url) #Get the page you want
 soup_get = bs4.BeautifulSoup(get.text) #Use soup to make the page pretty
 print soup_get.prettify() #Print HTMl
 
 #Print just the texts on the webpage
 print "Text:"
-print soup_get.get_text().split()
+#print soup_get.get_text().split()
+
+#for stuffs in soup_get.find_all('td'):
+#    print(stuffs.get('a'))
+
+#Get all the texts from the page and turn it into a single string
+all_texts = []
+for s in soup_get.find_all('a', text=True): 
+    all_texts += s  
+print "\n".join(all_texts) 
+    
 
 #Print all the links
 #print "Links:"
