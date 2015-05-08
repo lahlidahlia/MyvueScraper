eb7e611a2fd0ceec5d465b85fde725981a3822d1
Tin Huynh <tinlun123@gmail.com>
2015-05-07 21:51:13 -0700

    Sorted out a dictionary that contains the weights
    Key = Assignment type
    Value = Weight percent (Still in string right now, will need to convert)

--- MyvueScraper.py
+++ MyvueScraper.py
@@ -1,10 +1,13 @@
 import requests, bs4, sys, time
 
-USERNAME = raw_input('Name pls: ') 
+USERNAME = raw_input('User Name pls: ')
 PASSWORD = raw_input('Password pls: ')
 
-login_url = 'https://myvue.hsd.k12.or.us/Login_Student_PXP.aspx'
-destination_url = 'https://myvue.hsd.k12.or.us/Home_PXP.aspx'
+
+
+
+login_url = 'https://myvue.hsd.k12.or.us/Login_Student_PXP.aspx' #URL of the login page
+destination_url = 'https://myvue.hsd.k12.or.us/Home_PXP.aspx' #URL of where you want to go
 math_url = "https://myvue.hsd.k12.or.us/PXP_Gradebook.aspx?AGU=0&DGU=0&VDT=0&CID=866043FD-4A76-4AE3-A97E-EA655AF8117B&MK=3DCC8162-7F92-4317-AFBF-E5F464772BA0&OY=C26A5BDE-0387-4388-8855-1B81AE4C65F1&GP=B1C73921-DEB6-41E8-AA79-514267833B0A"
 
 #Poll the login page and get the appropriate form data
@@ -41,6 +44,7 @@ print soup_get.prettify() #Print HTMl
 
 #Print just the texts on the webpage
 print "Text:"
+print soup_get.get_text()
 #print soup_get.get_text().split()
 
 #for stuffs in soup_get.find_all('td'):
@@ -55,13 +59,21 @@ weightTr = soup_get.find('td', text="Weight")
 weightTable = weightTr.parent.parent
 i = 0
 print(weightTable)
-weightTabEntries = None
+weightTabEntries = [] #weightTabEntries will contain the tr which contains the td
 for x in weightTable.contents:
     print(x.name)
-    if x.name == 'td':
-        weightTabEntries.put(x)
-print(weightTabEntries)
-        
+    if x.name == 'tr':
+        weightTabEntries.append(x)
+
+
+weightTableDict = {} #Key: Assignment Type, Value: Weight percent
+
+for i in weightTabEntries:
+    ls = i.contents
+##    print unicode(ls[1].get_text())
+    weightTableDict[ls[1].get_text()] = ls[2].get_text()
+print weightTableDict
+    
 #for i in range(1, len(weightTable.contents) - 1):
 #    print(weightTable.contents[i])
 
